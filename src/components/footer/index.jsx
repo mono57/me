1ee989f77ec55550b726c0b08b90c5d1be212572
mono57 <aymaramono@gmail.com>
2020-02-18 22:27:34 +0100

    add footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+
+        <footer>
+            <div className="container-fluid text-center text-primary" style={{
+                // borderTop: "1px solid #000",
+                padding: '1rem'
+            }}>
+                <p>Copyright @ AMONO Aymar</p>
+            </div>
+        </footer>
+
+     );
+}
+ 
+export default Footer;
